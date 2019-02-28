4910d1d8f553d23fc3e401032bf33c7929b41bfc
luisfe <luisfernando_contreras@hotmail.com>
2019-02-27 21:54:08 -0600

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -82,7 +82,7 @@ public class Arrays{
   //
   // }
 
-  // GRAFICA LA FRECUENCIA DE UNA ENCUESTA
+  // MUESTRA LA FRECUENCIA DE UNA ENCUESTA
   public static void main(String[] args){
     int[] responses = { 1, 2, 5, 4, 3, 5, 2, 1, 3, 3, 1, 4, 3, 3, 3, 2, 3, 3, 2, 14 };
     int[] answer = new int[6];
